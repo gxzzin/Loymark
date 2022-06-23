95f8ce61c365bd33121793e9c95b26b8782770c5
gxzzin <gxzzin@outlook.com>
2022-06-23 16:56:45 -0600

    Add User model

---
+++ Backend/UsersControl/Models/User.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public DateTime Birthday { get; set; }
+
+    public int TelephoneNumber { get; set; }
+
+    public bool SendNews { get; set; }
+
+    public int CountryId { get; set; }
+}
